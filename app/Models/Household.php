dc0d6e66fc222b73ff2191ed6f46256d272c97de
Kiervin P. Dixon <kiervindixon@student.trimexcolleges.edu.ph>
2026-09-15 09:36:23 +0800

    Add models and factories for Blotter, Household, Official, and Resident with corresponding migrations

---
+++ app/Models/Household.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Household extends Model
+{
+    /** @use HasFactory<\Database\Factories\HouseholdFactory> */
+    use HasFactory;
+}
---
+++ database/factories/ResidentFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Resident;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Resident>
+ */
+class ResidentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
